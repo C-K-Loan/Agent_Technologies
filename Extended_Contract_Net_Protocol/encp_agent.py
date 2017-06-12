582eeaba2b9723d48a510ad9b1bd634d98893f9b
C-K-Loan <christian.kasim.loan@gmail.com>
2017-06-12 15:48:36 +0200

    Some cleanup and better interfacing with agent

--- Extended_Contract_Net_Protocol/encp_agent.py
+++ Extended_Contract_Net_Protocol/encp_agent.py
@@ -1,149 +1,165 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Jun  4 23:29:43 2017
+Created on Sun Jun  4 23:30:59 2017
 
 @author: loan
 """
 
-#import encp_manager as manager
+import numpy as np
 import math
-from collections import OrderedDict # so we can remember, in which order we set the bids
+import encp_agent as agent
+from encp_agent import Agent
+class Encp_manager():
 
-class Agent():
     instances = []
     id_counter = 0
-
-
-# x ∈ L.Orte – Position des Agenten zu Beginn einer Simulation.
-# @param location : (x,y)  X eight/vertical, Y Horizontal
-# k ∈ {1, ..., kmax} – Kapazität: drückt aus, wie viele Kilometer der Agent ohne Batteriewechsel fahren kann. Zu Beginn ist die Batterie voll geladen.
-# g ∈ {1, 2, 3, 4} - Geschwindigkeit: Anzahl der Kilometer, die der Agent in einem Zeitschritt fahren kann.
-#   p = (t1, ..., tm) mit ∀i,k ti ∈ T.ID und ti ≠ tk
-# Präferenzen: Nur Aufträge ti ∈ p kann der Agent bearbeiten. Immer wenn ein
-# solcher Auftrag entsteht, nimmt der Agent am korrespondierenden ECNP
-# teil. Entstehen 2 Aufträge ti ≠ tk gleichzeitig, dann drückt ti ≻ tk aus, dass der
-# Agent die Task ti präferiert, d.h. sie möglichst früh anfährt1.
-
-
-
-#@schedule, dictionary, Keys Managers:M-0,M-1,M-2.. , Values : next line
-#@schedeule[a][0] is bid that was send last to that manager 
-#@schedule[a][1] Boolean, IF true -> Bid send to Manager was DEFENTIVE, IF false -> Bid send to manager was PRE bid
-#@schedule [a][n] A list , 2 elements 
-    def __init__(self, id, x, k, g, p):
-        # def __init__(self, capacity, location, speed, preferences):
-        self.id = Agent.id_counter
-        self.location = x
-        self.capacity = k
-        self.speed = g
-        self.preferences = p
-        #self.schedule = {} 
-        Agent.instances.append(self)
-        Agent.id_counter += 1
-        self.schedule =  {}#key: manager , value0: bid value , value1 : bool defentive? 
-        
-
-    def __str__(self):
-        return "Agent ID: " + str(self.id) + "Location: " + str(self.location) + "Capacity:" + str(
-            self.capacity) + "Speed: " + str(self.speed) + "Preferences: " + str(self.preferences)
-
-
-# calculate using  |x1-x2| + |y1-y2|
-# todo Schedule berücksichtigen
-    def get_distance_to(self, location):
-        dist = abs(self.location[0] - location[0] )+ abs(self.location[1] - location[1])
-        return dist
-
-
-
-# calculate using  |x1-x2| + |y1-y2| , for location a to location b (tuples (x,y))
-    def calculate_distance(self,a,b):
-        
-        
-        
-        
-        dist = abs(a[0] - b[0] )+ abs(a[1] -b[1])
-        
-
-        return dist
-            
-
-#redeschedule and try better bid
-    def get_schedeuled_distance_to(self,manager_new):
-        #dist=self.get_distance_to(location)#todo take care of reschedule
-        last_manager_added=1
-        dist=0
-        for manager_it in self.schedule:#sum all previous bids 
-            dist+= self.schedule[manager_it][0]  #bid value  saved in thee dict
-            print("value is " +str(self.schedule))
-            last_manager_added= manager_it #in new Python versions, Dicts are orderd, so in the last iteration last_manager should become last manager added to dict
-                
-        print("last manager added" + str(last_manager_added.x[1]))
-        dist += self.calculate_distance (last_manager_added.x, manager_new.x)#calculate distance from last job on schedule to new job from manager_new
-        print("calculated Scheduled dist" + str(dist))
-            
-        return dist
-
-
-##edge from step 1 to 2,
-    def send_pre_bid(self,manager):
-        new_pre_bid = self.get_distance_to(manager.x)#ACTUALLY GET RESCHEDUELED DISTANCE
-               
-        if(not self.schedule == True):# if schedule is empty, this will return false -> no need is taking care of scheduling
-        #take care of sending the bid /actucally finding out if it is better
-            if manager  in self.schedule:# if this is the case, we have already send the manager a bid, now we must try to reschedule and send a better one
-                if (new_pre_bid< self.schedule[manager][0]):#only send the bid, when it was better than the old one
-                    print("AG-ID:"+str(self.id)+"sending Imrpoved bid :"+ str(new_pre_bid))
-                    self.schedule[manager]=[new_pre_bid,False] # key: manager, value is the bid the agent send him            
-                    manager.recv_pre_bid(self,new_pre_bid)          
-            else:
-                print("AG-ID:"+str(self.id)+"sending FIRST bid to the manager")
-                self.schedule[manager]= [new_pre_bid,False]#update schedule with a list
-                return new_pre_bid  
-
-        #handling reschedule, since schedule is not empty
-        scheduled_bid= self.get_schedeuled_distance_to(manager)
-        
-        print("TRYED SCHEDULED BID" +str(scheduled_bid))
-        print("AG-ID:"+ str(self.id)+ " Cannot send improved bid, Stoping Bidding with M-"+str(manager.id))
-        return math.inf# maybe smth else here.... maybe old bid again?
-
-# edge from step 3 to 5
-    def send_def_bid(self,manager):
-#        def_bid= get_dstance_to(manager.)
-        def_bid=self.get_distance_to(manager.x)#TODO TAKE CARE OF SCHEEDULEEs
-        
-        print("AG-ID:"+ str(self.id)+ "sending def bid Value:" + str(def_bid))
-        manager.recv_def_bid(self,def_bid)
-
-
-# edge from step 2 to 4
-#TODO re schedeuling 
-#called when recieving a pre reject, try to reschedule and send a better bid
-    def recv_pre_reject(self,manager):
-        self.send_pre_bid(manager)#try to send a better bid
-        
-
-
-# edge from step 2 to 3
-    def recv_pre_accept(self,manager):
-        print("Ag-ID "+  str(self.id)+"Recieved pre Accept")
-        self.send_def_bid(manager)
-
-
-# edge from step 5 to 6 TODO IMPLEMENT Rescheduleuing
-    def recv_def_accept(self,manager):
-        print("Ag-ID "+  str(self.id)+"Recieved def Accept")
-        self.schedule[manager][1]= True#set DEF bool
-
-        #todo, start doing job and def add to schedeule
-
-# edge from step 4 to 7 TODO IMPLEMENT RESCHEDEULING
-    def recv_def_reject(self,manager):
-        print("Ag-ID "+  str(self.id)+"Recieved Def Rej")
-        #todo remove Manager from scheduele completly
-        
-
     
 
+    #@param agents, a list of all created agents in the world    
+    #@param location, which location will be managed by this manager
+    def __init__(self, location, agents):
+        self.best_bid = [math.inf,None ]#best_bid(0) is value of Best bid, best_bid(1) is that corrosponding bidding Agent
+        self.bids = {}# Dictionary for Key:Agent Value:Tuple (BID,ID of agent)
+        self.x = location#which field does the ENCP manager manage
+        self.phase = 1#inital phase 1 of 2 
+        self.id = Encp_manager.id_counter
+        self.phase = 1
+        #self.pre_bid_rounds = 0
+        #self.agent_L = agents
+        Encp_manager.instances.append(self)
+        Encp_manager.id_counter += 1
+        self.best_bid_changed= False #inital 0 for first enter in loop, if  this is >0, it means we have a imrpoved bidder
+
+
+
+    def manage(self):
+    #function that manages ai/acting of manager
+    #inital call of this method, to start manager 
+    #once Task is managed, encp_manager instance will terminate through this method, free ressources?
+   #    for rounds in self.pre_bid_rounds#repeat for N pre bidding rounds
+        self.get_pre_bids()#collect al inital pre bids
+        print("M-"+str(self.id)+"P1_____________DONE WITH FIRST COLLECTION OF BIDS______")
+        
+        self.send_pre_accept()#send pre accet to agent, who is best_bid[0]    
+        print("M-"+str(self.id)+"P1_____________DONE WITH FIRST PRE ACCEPT________________________")#if best_bider changed, go to beginning of p1.2
+
+        self.send_pre_rejects()#send reject to every agent, who is not best_bid[0], they will answer withnwe bids, if bid is not better than best_bid[0]-> send defenitive bid()
+        print("M-"+str(self.id)+"P1.3_____________DONE WITH SENDING FIRST WAVE OF PRE REJECTS______")
+
+        if self.best_bid_changed == True :
+           self.reset_phase1()
+        else: 1+1
+        print("P2 _____NOBODY IMPROVED, STARTING PHASE 2____________")
+        self.send_def_reject()
+        print("M-"+str(self.id)+"_____DONE WITH SENDING DEF ACC_____")
+        self.send_def_accept()
+        print("M-"+str(self.id)+"_____DONE WITH SENDING DEF REJECTS_____")
+        
+
+
+
+    
+        
+    #todo, not really implemented
+    def reset_phase1(self):
+        print("________RESETTING PHASE 1_________")
+        self.send_pre_accept()#send pre accet to agent, who is best_bid[0]    
+        print("P1_____________DONE WITH FIRST PRE ACCEPT________________________")#if best_bider changed, go to beginning of p1.2
+
+        self.send_pre_rejects()#send reject to every agent, who is not best_bid[0], they will answer withnwe bids, if bid is not better than best_bid[0]-> send defenitive bid()
+        print("P1.3_____________DONE WITH SENDING FIRST WAVE OF PRE REJECTS______")
+        if self.best_bid_changed == true :
+           reset_phase1()#rekursiv weiter aufrufen, bis es keine verbesserung gibt
+        #PASTE REST OF MANAGE FUNCTION IN RESET
+    
+    #edge from 1 to 2, collect all pre bids, ask all agents for their bids
+    def get_pre_bids(self):
+        print("M-ID:"+str(self.id)+"Collecting bids...")
+        for agent_it in Agent.instances: #ANNOUNCE TASK/Inform Every Agent and get Pre Bid (1) Task Announcement and (2) Recieving end of Pre Bid
+            self.bids[agent_it] = (agent_it.send_pre_bid(self), agent_it.id)#aka Call for proposals(cfp),Use Agent as Key and Bid as Value
+            print("M-ID:"+str(self.id)+"Recieved bid :" + str(self.bids[agent_it][0]) + "from Agent ID:"+ str(self.bids[agent_it][1]))
+            
+        for agent_it in Agent.instances:#find best bidder, send him Pre Accept, Pre Reject to the rest
+            if(float(self.best_bid[0])> float(self.bids[agent_it][0])):
+                self.best_bid[0] = str(self.bids[agent_it][0])#actual value of the bid
+                self.best_bid[1] = str(self.bids[agent_it][0]) # id of the bidder
+            else : 1+1
+        print("M-ID:"+str(self.id)+"best Bid is:"+ str(self.best_bid[0] )+"with id :"+str(self.best_bid[1]))
+        
+        
+        
+        
+    #Inteface for agents, so they can bid, Should only be called by agent, if they can actually offer a better bid
+    def recv_pre_bid(self,agent_sender,bid):
+        print("M-ID:"+str(self.id)+ "  recv pre bid from ID:" +str(agent_sender.id)+ "value:" + str(bid)+"old best bid was"+str(self.best_bid[0]))
+        if(int(self.best_bid[0])< int(bid)):
+            self.send_pre_reject_to_best_bidder()
+            self.best_bid[0]= bid
+            self.best_bid[1]= agent_sender.id
+            print("New best bidder, sending pre Accept to him and pre rej to old best bidder")
+            agent_sender.recv_pre_accept(self)
+            self.best_bid_changed= True
+        else:
+            agent_sender.recv_def_reject()
+            self.best_bid_changed= False
+            
+
+
+
+    #send a pre reject to current best bidder
+    def send_pre_reject_to_best_bidder(self):
+        for agent_it in Agent.instances:
+            if int(agent_it.id) == int(self.best_bid[1]):
+                agent_it.recv_pre_reject(self)
+                
+    #edge from step 2 to 4 , Send pre reject to every agent, who is not best bidder
+    def send_pre_rejects(self):
+        for agent_it in Agent.instances:
+            if(int(agent_it.id) !=int(self.best_bid[1])):
+                print("M-ID:"+str(self.id)+"sending pre reject to agent ID : "+ str(agent_it.id) )
+                agent_it.recv_pre_reject(self)
+
+    #edge from 2 to 4 
+    #send pre accept to best bidder
+    def send_pre_accept(self):
+        for agent_it in Agent.instances:
+            if(int(agent_it.id) ==int(self.best_bid[1])):
+                print("M-ID:"+str(self.id)+"sending pre Acc to agent ID : "+ str(agent_it.id) )
+                agent_it.recv_pre_accept(self)
+ 
+    
+    
+    
+    #Edge from 3 to 5| @ param agent, agent that sends the def bid, this will recieve best_bid and then ask all agents for 
+    def recv_def_bid(self,agent_sender,def_bid):
+        print("M-ID:"+str(self.id)+"recieved def_bid!, Val:"+str(def_bid)+"from agent ID: " + str(agent_sender.id))
+        if int(self.best_bid[0])<= def_bid:
+            self.best_bid[0] = def_bid
+        
+    
+    #edge from 5 to 6 -> END OF protocoll
+    def send_def_accept(self):
+        for agent_it in Agent.instances:
+            if(int(agent_it.id) !=int(self.best_bid[1])):
+                print("M-ID:"+str(self.id)+"sending DEF reject to agent ID : "+ str(agent_it.id) )
+                agent_it.recv_def_reject(self)
+
+    #edge from 5 to 7 -> END of protocoll
+    def send_def_reject(self):
+        for agent_it in Agent.instances:
+            if(int(agent_it.id) ==int(self.best_bid[1])):
+                print("M-ID:"+str(self.id)+"sending def Acc to agent ID : "+ str(agent_it.id) )
+                agent_it.recv_def_accept(self)
+ 
+
+test_agent1= Agent(5,(0,0),15,20, [0])
+test_agent2= Agent(5,(3,3),15,20, [0])
+test_agent3= Agent(5,(1,1),15,20, [0])
+test_agent4= Agent(5,(4,4),15,20, [0])
+
+agent_list=[test_agent1,test_agent2,test_agent3,test_agent4]
+manager_t= Encp_manager((2,2),agent_list)
+#manager_t.recv_pre_bids()
+manager_t.manage()
+#print(test_agent1.get_distance_to((5,5)))
