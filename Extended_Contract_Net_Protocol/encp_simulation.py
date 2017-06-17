a88b9097c49d08d7b36244cf82045acc3e1e68ad
C-K-Loan <christian.kasim.loan@gmail.com>
2017-06-17 21:24:22 +0200

    Improved simulation for multiple agents at same time
    #A list, whoose elements are Lists of Managers 
    #list [0] is a list of managers, who should be initated for Time 0
    #list [n] is list of managers, who should be initated at time N
    manager_release_time_list= []

--- Extended_Contract_Net_Protocol/encp_simulation.py
+++ Extended_Contract_Net_Protocol/encp_simulation.py
@@ -67,7 +67,7 @@ class Initiator():
     #announce Task , Create ENCP instance for that task
     def announce_task(self):
         if self.task.t == time_global: #only Release Task when world is at that time
-         
+            
             print("TASK FOR " + str(self.task.x)+"initiated!")
             manager_t = Encp_manager(self.task.x,agent_list)
             print ("IN SIMULATION")
@@ -79,16 +79,63 @@ class Initiator():
          #   print ("World time is " + str(time_global) )
 
 
-def simulate(t, n=0):
+def simulate(t,manager_release_time_list):
     global time_global
     time_global = 0
     #every time iteration, tasks get announced and if the time is right, Encp manager will be created
-    for t in range(t):
-        for initiator in Initiator.instances:
-            initiator.announce_task()
+    for time in range(t):
+        if (manager_release_time_list[time]!= []):# if list is not empty at position, then there are managers to simulate
+            while(confirm_no_more_managers_to_simulate(time) == False):#Only simulate, if managers for that time are unfinished
+                
+                #Phase 1STEP 2 collect Pre Bids
+                for manager_sim in manager_release_time_list[time]:#simulate evermanger for time 
+                    if manager_sim.finished == False and manager_sim.phase == 1: # ALPHA IF
+                        manager_sim.get_pre_bids()
+                
+                #Phase1#STEP 3 And 4 Evaluate Pre Bids and send Responses to Agents, Agents will react with DEF bids
+                for manager_sim in manager_release_time_list[time]:#simulate evermanger for time 
+                    if manager_sim.finished == False and manager_sim.phase == 1: # ALPHA IF
+                        manager_sim.find_and_set_best_bidder()                        
+                        manager_sim.send_pre_accepts()#after this, def bid is send automatically
+                        manager_sim.send_pre_rejects()#after this, everyone who is rejected whill send a new pre_bid, if they can imrpove
+                
+
+                #wait 1 s for agents to answer 
+
+                #update phase status, check if there is a neew best bidder, if yes -> phase 1, if best bidder didint change ->phase 2 (sending DEF Rej/Acc)
+                for manager_sim in manager_release_time_list[time]:
+                    if manager_sim.finished == False:
+                        manager_sim.set_phase()
+                
+                    
+                #Phase 2 STEP 3 Agents havesend Def bids by now, if best_bidder didint changed manager sends def Accepts and Def Rejects 
+                for manager_sim in manager_release_time_list[time]:#simulate evermanger for time 
+                    if manager_sim.finished == False and manager_sim.phase == 2: # ALPHA IF
+                        manager_sim.send_def_reject()
+                        manager_sim.send_def_accept()
+
+                
+                    
+        
+        
         time_global += 1
                 
     
+
+
+
+#confirm if there are managers to simulate for time t
+#return FALSE,if there are unfinished managers for time T
+#return TRUE, if every manager for time T  is finished (def acc/rej send)
+def confirm_no_more_managers_to_simulate(time):
+    result = True
+    for manager in manager_release_time_list[time]:
+        if manager.finished == False:
+            result = False
+    return result
+        
+    
+
 
 #bulding agent: 
 #id, location, capacity ,speed, preferences[]
@@ -105,17 +152,29 @@ print ("PRE MANAGER CONSTRUCT")
 
 
 
+#A list, whoose elements are Lists of Managers 
+#list [0] is a list of managers, who should be initated for Time 0
+#list [n] is list of managers, who should be initated at time N
+manager_release_time_list= []
+manager_release_time_list.insert(0,[])
+manager_release_time_list.insert(1,[])
+manager_release_time_list.insert(2,[])
+manager_release_time_list.insert(3,[])
+manager_release_time_list.insert(4,[])
+manager_release_time_list.insert(5,[Encp_manager((0,4),agent_list),Encp_manager((3,0),agent_list)])
+#print ("relase time lsit :" + str(manager_release_time_list))
+simulate(10,manager_release_time_list)
 
-task1= Task(5,(0,4))#should be won my agent 1
 
-task2= Task(5,(3,0))#should be won by agent 2 
+#task1= Task(5,(0,4))#should be won my agent 1
+
+#task2= Task(5,(3,0))#should be won by agent 2 
 #task3= Task(4,(7,7))#should be won by ag3
 #test_agent= agent.Agent(5,(0,0),15,20, [0])
-initiator1 = Initiator(task1)
-initiator2 = Initiator(task2)
+#initiator1 = Initiator(task1)
+#initiator2 = Initiator(task2)
 #initiator3 = Initiator(task3)
 
 #init_world(5,5)
-simulate(10)
 
 #test_manager.recv_pre_bids()
