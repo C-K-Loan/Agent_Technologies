6107ee5fcef3e9ab2064daa5e94de3c4a3c4b87b
C-K-Loan <christian.kasim.loan@gmail.com>
2017-07-14 17:44:10 +0200

    Update I_Bundle_Auction.py

--- Combinatorial_Auctions_I_Bundle/I_Bundle_Auction.py
+++ Combinatorial_Auctions_I_Bundle/I_Bundle_Auction.py
@@ -7,13 +7,28 @@ class Auction():
     instances = []
     id_counter = 0
 
-    def __init__(self, ):
+    def __init__(self,bundle_list,agent_list ):
         self.id = Auction.id_counter
+        self.agent_list = agent_list
+        self.bundle_list = bundle_list
         Auction.instances.append(self)
         Auction.id_counter += 1
-   
+        self.init_price_list()
+        
+        
 
+    def start_auction(self):
+        
+        self.advertise_bundles()#start by sending all agent pricelist
+        
+ 
+    def advertise_bundles(self):
+        for agent in self.agent_list:
+            agent.recv_price_list(self.price_list)
+        
+        
 
+       
     def hello_auction(self):
         print("Hello from Auction!")
         
@@ -22,3 +37,9 @@ class Auction():
         test_agent.hello_agent()
 
 
+    def init_price_list(self):
+        #init all bundle prices to 0
+        self.price_list = {}
+        for bundle in self.bundle_list:
+            self.price_list[bundle]= 0
+        
