e99b7561854399711c497de509bf3c4cb739a51b
C-K-Loan <christian.kasim.loan@gmail.com>
2017-07-16 21:00:05 +0200

    Update I_Bundle_Auction.py

--- Combinatorial_Auctions_I_Bundle/I_Bundle_Auction.py
+++ Combinatorial_Auctions_I_Bundle/I_Bundle_Auction.py
@@ -13,7 +13,9 @@ class Auction():
         self.bundle_list = bundle_list
         Auction.instances.append(self)
         Auction.id_counter += 1
+        self.agents_bid_list = {}# dictonary with key= Agent, Value is the bid list from that agent
         self.init_price_list()
+        self.bids_recieved = 0 
         
         
 
@@ -33,15 +35,20 @@ class Auction():
         print("Hello from Auction!")
         
 
-    def recv_bid_list(x,bid_list,agent):
-        #recviece the bids of an agent
-        print("AUCTION: Recieved bid "+ str(bid_list[0].name) + "from agent" + str(x.id))
-        
-  
+    def recv_bid_list(agent,bid_list,self):
+        self.bids_recieved += 1 
+        #recviece the bids of an agent and safe the bids until we have all bids recieved or Timeout
+        self.agents_bid_list[agent] = bid_list# for Every Agent entry in DIct, there is a List of his XOR bids
+        print("AUCTION: Recieved bid "+ str(bid_list[0].name) + "from agent" + str(agent.id) + "for price" + str(self.price_list[bid_list[0]]))
+        if self.bids_recieved == len(self.agent_list):
+            print("recieved all bids , decidin..")
+            
+    def find_revenue_maximizing_distribution(self):
+        #calc all possible combinations, check  revenue for all and pick most profitable and distribute evenly
+        1+1
 
-    def find_
-      
-        
+
+
     def print_bid_list(self,bid_list):
         for bundle in bid_list:
             print ("extra print" + bundle.name)
